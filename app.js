04610d62f9e83094856e0efbffc83dd71e7f9da7
azizsige <kurniawan00azizsige@gmail.com>
2022-06-15 12:16:48 +0700

    update: modified style

--- app.js
+++ app.js
@@ -1,50 +1,114 @@
+// (function () {
+//   let container = document.querySelector(".container");
+//   let display = document.querySelector(".display");
+//   let btn = document.getElementById("spin");
+
+//   let deg = 0;
+
+//   const dataZones = {
+//     1: "1",
+//     2: "2",
+//     3: "3",
+//     4: "4",
+//     5: "5",
+//     6: "6",
+//     7: "7",
+//     8: "8",
+//   };
+
+//   var size = Object.keys(dataZones).length;
+
+//   let zoneSize = 45;
+//   console.log(zoneSize);
+
+//   function handleWin(acutalDeg) {
+//     let winner = Math.ceil(acutalDeg / zoneSize);
+//     display.innerHTML = dataZones[winner];
+//     console.log(winner);
+//   }
+
+//   btn.addEventListener("click", function () {
+//     display.innerHTML = "-";
+//     deg = Math.floor(3000 + Math.random() * 3000);
+//     // Set the transition on the wheel
+//     container.style.transition = "all 8s ease-out";
+//     // let numberDeg = deg % 360;
+//     container.style.transform = `rotate(${deg}deg)`;
+//     console.log(deg);
+//     // number += Math.floor(5000 + Math.random() * 5000);
+//   });
+
+//   container.addEventListener("transitionend", function () {
+//     // alert("Yeayy");
+//     // deg = Math.floor(3000 + Math.random() * 3000);
+//     const actualNumber = (deg % 360) / size;
+//     container.style.transition = "none";
+//     container.style.transform = `rotate(${actualNumber}deg)`;
+//     handleWin(actualNumber);
+//     // console.log(actualNumber);
+//   });
+// })();
+
+// Immediately invoked function expression
+// to not pollute the global scope
 (function () {
-  let container = document.querySelector(".container");
-  let display = document.querySelector(".display");
-  let btn = document.getElementById("spin");
+  const wheel = document.querySelector(".wheel");
+  const startButton = document.querySelector(".button");
+  const display = document.querySelector(".display");
 
   let deg = 0;
+  let zoneSize = 30; // deg
 
-  const dataZones = {
-    1: "1",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
+  // Counter clockwise
+  const symbolSegments = {
+    1: "Iphone 13 Pro",
+    2: "COBA LAGI",
+    3: "5.000.000",
+    4: "50.000",
+    5: "25.000",
+    6: "FREE SPIN",
+    7: "500.000",
+    8: "10.000",
+    9: "COBA LAGI",
+    10: "1.000.000",
+    11: "100.000",
+    12: "5.000",
   };
 
-  var size = Object.keys(dataZones).length;
+  const handleWin = (actualDeg) => {
+    const winningSymbolNr = Math.ceil(actualDeg / zoneSize);
+    display.innerHTML = symbolSegments[winningSymbolNr];
+  };
 
-  let zoneSize = 45;
-  console.log(zoneSize);
-
-  function handleWin(acutalDeg) {
-    let winner = Math.ceil(acutalDeg / zoneSize);
-    display.innerHTML = dataZones[winner];
-    console.log(winner);
-  }
-
-  btn.addEventListener("click", function () {
+  startButton.addEventListener("click", () => {
+    // Reset display
     display.innerHTML = "-";
-    deg = Math.floor(3000 + Math.random() * 3000);
+    // Disable button during spin
+    startButton.style.pointerEvents = "none";
+    // Calculate a new rotation between 5000 and 10 000
+    deg = Math.floor(5000 + Math.random() * 5000);
     // Set the transition on the wheel
-    container.style.transition = "all 8s ease-out";
-    // let numberDeg = deg % 360;
-    container.style.transform = `rotate(${deg}deg)`;
-    console.log(deg);
-    // number += Math.floor(5000 + Math.random() * 5000);
+    wheel.style.transition = "all 10s ease-out";
+    // Rotate the wheel
+    wheel.style.transform = `rotate(${deg}deg)`;
+    // Apply the blur
+    wheel.classList.add("blur");
   });
 
-  container.addEventListener("transitionend", function () {
-    // alert("Yeayy");
-    // deg = Math.floor(3000 + Math.random() * 3000);
-    const actualNumber = (deg % 360) / size;
-    container.style.transition = "none";
-    container.style.transform = `rotate(${actualNumber}deg)`;
-    handleWin(actualNumber);
-    // console.log(actualNumber);
+  wheel.addEventListener("transitionend", () => {
+    // Remove blur
+    wheel.classList.remove("blur");
+    // Enable button when spin is over
+    startButton.style.pointerEvents = "auto";
+    // Need to set transition to none as we want to rotate instantly
+    wheel.style.transition = "none";
+    // Calculate degree on a 360 degree basis to get the "natural" real rotation
+    // Important because we want to start the next spin from that one
+    // Use modulus to get the rest value
+    const actualDeg = deg % 360;
+    // Set the real rotation instantly without animation
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+    // Calculate and display the winning symbol
+    handleWin(actualDeg);
   });
 })();
