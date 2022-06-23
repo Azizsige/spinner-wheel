ad8a7427aedd9b80c325dcf85db23b9ce8c8a0ef
azizsige <kurniawan00azizsige@gmail.com>
2022-06-23 15:16:52 +0700

    update: test clear cache

--- app.js
+++ app.js
@@ -28,34 +28,23 @@ const handleWin = (actualDeg) => {
 };
 
 startButton.addEventListener("click", () => {
-  // Reset display
   display.innerHTML = "-";
-  // Disable button during spin
   startButton.style.pointerEvents = "none";
-  // Calculate a new rotation between 5000 and 10 000
+
+  // ubah angka 6 sesuai value symbolSegments
   deg =
     Math.floor((5000 + Math.random() * 5000) / 360) * 360 + 6 * zoneSize - 15;
-  // Set the transition on the wheel
+
   wheel.style.transition = "all 10s ease-out";
-  // Rotate the wheel
   wheel.style.transform = `rotate(${deg}deg)`;
-  // Apply the blur
   wheel.classList.add("blur");
 });
 
 wheel.addEventListener("transitionend", () => {
-  // Remove blur
   wheel.classList.remove("blur");
-  // Enable button when spin is over
   startButton.style.pointerEvents = "auto";
-  // Need to set transition to none as we want to rotate instantly
   wheel.style.transition = "none";
-  // Calculate degree on a 360 degree basis to get the "natural" real rotation
-  // Important because we want to start the next spin from that one
-  // Use modulus to get the rest value
   const actualDeg = deg % 360;
-  // Set the real rotation instantly without animation
   wheel.style.transform = `rotate(${actualDeg}deg)`;
-  // Calculate and display the winning symbol
   handleWin(actualDeg);
 });
